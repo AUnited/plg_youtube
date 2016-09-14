2b244149c911e4145a59c5bc431f7bc588648292
dragonzx <DragonZX@yandex.ru>
2016-09-14 23:31:55 +0300

    Comments and spaces review

--- youtube.php
+++ youtube.php
@@ -1,70 +1,43 @@
 <?php
-/**
-* @ 2013 Benj Golding. All rights reserved.
-* @GNU/GPL licence
-*
-*/
+# @ 2013 Benj Golding. All rights reserved. GNU/GPL licence
+# @ 2014-2016 Vitaliy Zhukov. All rights reserved. GNU/GPL v3 licence
 
-// Assert file included in Joomla!
+# Assert file included in Joomla!
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
 jimport( 'joomla.plugin.plugin' );
 
-/**
-* YouTube Content Plugin
-*
-*/
+#YouTube Content Plugin
 class plgContentYoutubePlugin extends JPlugin
 {
 
-	/**
-	* Ctor
-	*
-	* @param object $subject The object to observe
-	* @param object $params The object that holds the plugin parameters
-	*/
 	function PluginYoutube( &$subject, $params )
 	{
 		parent::__construct( $subject, $params );
 	}
 
-	/**
-	* Example prepare content method
-	*
-	* Method is called by the view
-	*
-	* @param object The article object. Note $article->text is also available
-	* @param object The article params
-	* @param int The 'page' number
-	*/
 	function onContentPrepare ( $context, &$article, &$params, $page = 0)
 		{
 		global $mainframe;
-		
 
 		if ( JString::strpos( $article->text, '{youtube}' ) === false || JString::strpos( $article->text, '{youpl}' ) === false) {
 		return true;
 		}
 
 		$article->text = preg_replace_callback('|{youtube}(.*){\/youtube}|',function ($match){return $this->embedVideo($match[1]);}, $article->text);
-
         $article->text = preg_replace_callback('|{youpl}(.*){\/youpl}|',function ($match){return $this->plVideo($match[1]);}, $article->text);
-
-
-
 		return true;
-	
 	}
 
 	function embedVideo($vCode)
 	{
-
 	 	$params = $this->params;
 
 		$width = $params->get('width', 425);
 		$height = $params->get('height', 344);
         $confidence = $params->get('confidence', 0);
         $fullscreen = $params->get('fullscreen', 1);
+
         #Logic
         if($confidence) $url='www.youtube-nocookie.com'; else $url='www.youtube.com';
         if($fullscreen) $fscr=' allowfullscreen'; else $fscr='';
@@ -74,18 +47,17 @@ class plgContentYoutubePlugin extends JPlugin
 
 	function plVideo($vCode)
     {
-
         $params = $this->params;
 
         $width = $params->get('width', 425);
         $height = $params->get('height', 344);
         $confidence = $params->get('confidence', 0);
         $fullscreen = $params->get('fullscreen', 1);
+
         #Logic
         if($confidence) $url='www.youtube-nocookie.com'; else $url='www.youtube.com';
         if($fullscreen) $fscr=' allowfullscreen'; else $fscr='';
 
         return '<iframe width="'.$width.'" height="'.$height.'" src="https://'.$url.'/embed/videoseries?list='.$vCode.'" frameborder="0" '.$fscr.'></iframe>';
     }
-
 }
